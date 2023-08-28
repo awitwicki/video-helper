7dc05dfcf7c73dea4c08294184c2f7b2b301f604
Andrew Witwicki <vitvitskiy3@gmail.com>
2023-08-28 22:27:19 +0200

    Add github link

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,27 @@
 import { Suspense } from 'preact/compat'
 import FFmpegComponent from "./components/FFmpegComponent";
 import FileExportParamsComponent from "./components/FileExportParamsComponent";
+import { FaGithub } from "react-icons/fa";
 
 export default function () {
   return (
     <div className="container max-w-screen-2xl mx-auto p-10 min-w-min">
-      <h1 className="text-4xl font-bold mb-4">Simple converter</h1>
-      <Suspense fallback={<p>Loading...</p>}>
-          <div className="flex gap-4">
-            <FFmpegComponent />
-            <FileExportParamsComponent />
-          </div>
-      </Suspense>
+        <div className="flex items-center mb-4">
+            <h1 className="text-4xl font-bold mr-2">Simple converter</h1>
+            <a
+                href="https://github.com/awitwicki/video-helper"
+                target="_blank"
+                rel="noopener noreferrer"
+            >
+                <FaGithub size={30} />
+            </a>
+        </div>
+        <Suspense fallback={<p>Loading...</p>}>
+            <div className="flex gap-4">
+              <FFmpegComponent />
+              <FileExportParamsComponent />
+            </div>
+        </Suspense>
     </div>
   )
 }
