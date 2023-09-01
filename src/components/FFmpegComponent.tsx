d47e48a62dad80591ca798ee9c21ba4d8cfe50cb
Andrew Witwicki <vitvitskiy3@gmail.com>
2023-09-01 21:18:07 +0200

    Add media processing animation to button

--- src/components/FFmpegComponent.tsx
+++ src/components/FFmpegComponent.tsx
@@ -8,6 +8,7 @@ import { useState } from 'react'
 import { settingsAtom } from '../atoms/exportSettings'
 import { GenerateFfmpegParams } from  '../helpers/ExportSettingsFFmpegParamsBuilder'
 import {ffmpegCommandAtom} from "../atoms/ffmpegCommand";
+import {FaSpinner} from "react-icons/fa";
 
 function FFmpegComponent() {
   const [input, setInput] = useState<File | null>(null)
@@ -19,6 +20,7 @@ function FFmpegComponent() {
 
   const [settings, setFfmpegSettings] = useAtom(settingsAtom)
   const [fileSizeExceeded, setFileSizeExceeded] = useState(false);
+  const [isProcessing, setIsProcessing] = useState(false);
   const [fileSizeWarning, setFileSizeWarning] = useState(false);
   const [clickToCopyText, setClickToCopyText] = useState('Click to copy');
   const maxFileSizeForNormalWork = 10; // MB
@@ -62,6 +64,8 @@ function FFmpegComponent() {
       return
     }
 
+    setIsProcessing(true)
+    
     try {
       const ffmpeg = ffmpegRef.current
       const inputFilename = settings.inputFileName!
@@ -80,6 +84,8 @@ function FFmpegComponent() {
     } catch (error) {
       console.error('Error converting video:', error)
     }
+
+    setIsProcessing(false)
   }
 
   const handleFileChange = async (event: Event) => {
@@ -115,7 +121,6 @@ function FFmpegComponent() {
           for="formFile"
           class="my-4 mb-2 inline-block text-neutral-700 dark:text-neutral-200"
         >
-          Default file input example
         </label>
         <input
           class="relative m-0 block w-full min-w-0 flex-auto rounded border border-solid border-neutral-300 bg-clip-padding px-3 py-[0.32rem] text-base font-normal text-neutral-700 transition duration-300 ease-in-out file:-mx-3 file:-my-[0.32rem] file:overflow-hidden file:rounded-none file:border-0 file:border-solid file:border-inherit file:bg-neutral-100 file:px-3 file:py-[0.32rem] file:text-neutral-700 file:transition file:duration-150 file:ease-in-out file:[border-inline-end-width:1px] file:[margin-inline-end:0.75rem]  dark:border-neutral-600 dark:text-neutral-200 dark:file:bg-neutral-700 dark:file:text-neutral-100"
@@ -141,9 +146,10 @@ function FFmpegComponent() {
         </p>
       </div>
       {input && (
-        <button class="btn" onClick={transcode}>
-          Transcode to mp4
-        </button>
+          <button className="btn" onClick={transcode}>
+            {isProcessing && <FaSpinner className="animate-spin"/>}
+            {isProcessing ? "Processing..." : "Convert"}
+          </button>
       )}
       <p className="my-4">
         {progress}
