130360d9a10560064b0b1520148cbaf97ef0c87b
Andrew Witwicki <vitvitskiy3@gmail.com>
2023-09-14 07:00:39 +0200

    Add download FFmpeg link

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,7 @@ import { FaGithub } from "react-icons/fa";
 export default function () {
   return (
     <div className="container max-w-screen-2xl mx-auto p-10 min-w-min">
-        <div className="flex items-center mb-4">
+        <div className="flex items-center mb-2">
             <h1 className="text-4xl font-bold mr-2">Simple converter</h1>
             <a
                 href="https://github.com/awitwicki/video-helper"
@@ -16,6 +16,7 @@ export default function () {
                 <FaGithub size={30} />
             </a>
         </div>
+        <a href="https://ffmpeg.org/download.html" target="_blank" class="mb-4">Download FFmpeg</a>
         <Suspense fallback={<p>Loading...</p>}>
             <div className="flex gap-4">
               <FFmpegComponent />
