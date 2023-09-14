a507e9504ce457de5036809d67724f98c68d82ba
Andrew Witwicki <vitvitskiy3@gmail.com>
2023-09-14 07:50:30 +0200

    Improvement: use daisyUI file input component

--- src/components/FFmpegComponent.tsx
+++ src/components/FFmpegComponent.tsx
@@ -137,12 +137,10 @@ function FFmpegComponent() {
           class="my-4 mb-2 inline-block text-neutral-700 dark:text-neutral-200"
         >
         </label>
-        <input
-          class="relative m-0 block w-full min-w-0 flex-auto rounded border border-solid border-neutral-300 bg-clip-padding px-3 py-[0.32rem] text-base font-normal text-neutral-700 transition duration-300 ease-in-out file:-mx-3 file:-my-[0.32rem] file:overflow-hidden file:rounded-none file:border-0 file:border-solid file:border-inherit file:bg-neutral-100 file:px-3 file:py-[0.32rem] file:text-neutral-700 file:transition file:duration-150 file:ease-in-out file:[border-inline-end-width:1px] file:[margin-inline-end:0.75rem]  dark:border-neutral-600 dark:text-neutral-200 dark:file:bg-neutral-700 dark:file:text-neutral-100"
-          type="file"
-          accept="video/*"
-          onChange={handleFileChange}
-        />
+        <input type="file" 
+               accept="video/*"
+               className="file-input file-input-bordered w-full my-2"
+               onChange={handleFileChange}/>
         {fileSizeExceeded && (
             <span className="text-red-700">
               File size exceeded the WASM limit of {maxFileSize} MB
